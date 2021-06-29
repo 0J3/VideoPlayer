b8a5c50b3a620ef442ce96fee58716660653963d
0J3 <GH@wafflepowered.com>
2021-06-29 03:10:56 +0200

    remove d

--- d/UUID.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const gen: () => string;
-//# sourceMappingURL=../ts/js/UUID.d.ts.map
--- d/getHtml.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const generateWithArgs: (args: {
-    [x: string]: any;
-}) => string;
-export declare const getUrl: (path?: string) => string;
-export declare const generateUnique: (name: any) => any;
-declare const _default: (path?: string, isIe?: boolean) => string;
-export default _default;
-//# sourceMappingURL=../ts/js/getHtml.d.ts.map
