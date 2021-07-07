4348174504654d663d370d4f34f9e0ec04a4b911
0J3 <GH@wafflepowered.com>
2021-07-07 13:18:22 +0200

    a

--- src/components/CursorManager.ts
+++ src/components/CursorManager.ts
@@ -10,38 +10,45 @@ export class CursorMan {
 
 	constructor(i: number) {
 		this.InternalShowCursor();
-		setInterval(() => {
-			this.v = this.v - i;
-			if (this.v < 0) {
-				this.v = 0;
-				if (!this.isHidden) this.InternalHideCursor();
-			} else {
-				if (this.isHidden) this.InternalShowCursor();
-			}
-		}, i);
+		if (typeof document !== 'undefined')
+			setInterval(() => {
+				this.v = this.v - i;
+				if (this.v < 0) {
+					this.v = 0;
+					if (!this.isHidden) this.InternalHideCursor();
+				} else {
+					if (this.isHidden) this.InternalShowCursor();
+				}
+			}, i);
 	}
 	InternalShowCursor() {
-		this.isHidden = false;
-		this.ShowCursor();
-		document.body.setAttribute('data-isHidden', 'false');
-		console.log('Showing Cursor');
+		if (typeof document !== 'undefined') {
+			this.isHidden = false;
+			this.ShowCursor();
+			document.body.setAttribute('data-isHidden', 'false');
+			console.log('Showing Cursor');
+		}
 	}
 	InternalHideCursor() {
-		this.isHidden = true;
-		this.HideCursor();
-		document.body.setAttribute('data-isHidden', 'true');
-		console.log('Hiding Cursor');
+		if (typeof document !== 'undefined') {
+			this.isHidden = true;
+			this.HideCursor();
+			document.body.setAttribute('data-isHidden', 'true');
+			console.log('Hiding Cursor');
+		}
 	}
 	Move() {
 		this.v = this.timeout;
 	}
 
 	registerListeners() {
-		console.log('Registering Event Listeners');
+		if (typeof document !== 'undefined') {
+			console.log('Registering Event Listeners');
 
-		document.body.addEventListener('keydown', () => this.Move());
-		document.body.addEventListener('keyup', () => this.Move());
-		document.body.addEventListener('mousemove', () => this.Move());
+			document.body.addEventListener('keydown', () => this.Move());
+			document.body.addEventListener('keyup', () => this.Move());
+			document.body.addEventListener('mousemove', () => this.Move());
+		}
 	}
 }
 export const cursorMan = new CursorMan(100);
--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from 'preact';
+import { Component, h, createRef } from 'preact';
 import get from './getUrl';
 
 import Cursor from './CursorManager';
@@ -20,10 +20,51 @@ class Controls extends Component<{
 
 export class Player extends Component<{
 	Path: string;
+	isPlaying?: boolean;
 }> {
+	ref = createRef();
+
+	togglePlayback() {
+		const x = this.ref.current;
+
+		if (!x.paused) {
+			x.pause();
+		} else {
+			x.play();
+		}
+		this.props.isPlaying = !x.paused;
+
+		x.setAttribute('data-isPlaying', (!x.paused).toString());
+		typeof document !== 'undefined'
+			? document.body.setAttribute('data-isPlaying', (!x.paused).toString())
+			: null;
+	}
+	componentDidMount() {
+		this.togglePlayback();
+		this.ref.current.addEventListener('click', () => this.togglePlayback());
+		if (typeof document !== 'undefined')
+			setInterval(() => {
+				const x = this.ref.current;
+				if (!x) return;
+
+				x.setAttribute('data-isPlaying', (!x.paused).toString());
+				document.body.setAttribute('data-isPlaying', (!x.paused).toString());
+
+				x.setAttribute('data-isMuted', x.muted);
+				document.body.setAttribute('data-isMuted', x.muted);
+			}, 100);
+	}
 	render() {
 		return (
 			<div id="player" class={c('playercomponent')}>
+				<video
+					class={c('video')}
+					id="VideoPlayerEl"
+					ref={this.ref}
+					data-isplaying="false"
+				>
+					<source src={this.props.Path} />
+				</video>
 				<Controls Funcs={{}} />
 			</div>
 		);
